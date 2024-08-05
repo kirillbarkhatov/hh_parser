6ac0855e9f9b26a8b97e4407f83453ad8c239ee6
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-08-06 00:39:24 +0300

    Общий класс для работы с файлами

---
+++ src/file_worker.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class FileWorker(ABC):
+    """Абстрактный класс, предъявляющий требования к подклассам, работающими с файлами"""
+
+    @abstractmethod
+    def save_to_file(self, *args: Any, **kwargs: Any) -> Any:
+        """Общий функционал для сохранения данных в файл"""
+
+        pass
+
+    @abstractmethod
+    def add_to_file(self, *args: Any, **kwargs: Any) -> Any:
+        """Общий функционал для добавления данных в файл"""
+
+        pass
+
+    @abstractmethod
+    def get_from_file(self, *args: Any, **kwargs: Any) -> Any:
+        """Общий функционал для получения данных из файла"""
+
+        pass
+
+    @abstractmethod
+    def delete_from_file(self, *args: Any, **kwargs: Any) -> Any:
+        """Общий функционал для удаления данных из файла"""
+
+        pass
