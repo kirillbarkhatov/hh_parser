0fba7f10729843380ce8acbb679cc95a77d5de75
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-08-14 19:44:12 +0300

    Функция анализа файлов в data/

---
+++ main3.py
@@ -0,0 +1,4 @@
+from src.utils import file_data_info
+
+
+print(file_data_info())
---
+++ src/utils.py
@@ -0,0 +1,28 @@
+import os
+from src.logger_decorators import func_call_logging
+
+
+from src.file_processing.json_worker import JSONWorker
+from src.vacancy_processing.vacancy import Vacancy
+
+
+@func_call_logging
+def file_data_info() -> list:
+    """Функция для получения данных о файлах в папке data"""
+
+    path = "data/"
+    files = []
+    for dir_entry in os.scandir(path):
+        if dir_entry.is_file():
+            files.append(dir_entry.name)
+    print(f"В папке {path} содержится {len(files)} файл(а)(ов):")
+    for file in files:
+        try:
+            checking_file = JSONWorker(file)
+            file_data = checking_file.get_from_file()
+            vacancies = Vacancy.list_id_vacancies(file_data)
+            print(f"{file} - содержит {len(vacancies)} вакансий")
+        except TypeError:
+            print(f"{file} - файл не содержит данных о вакансиях или формат файла не поддерживается")
+
+    return files
