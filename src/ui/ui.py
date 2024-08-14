7a257d0bdb989b1f8a33b67f43dbdff06ffe11e3
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-08-14 19:43:44 +0300

    Заготовка для функций ui

---
+++ src/ui/ui.py
@@ -0,0 +1,13 @@
+import os
+
+
+class UI:
+
+    @staticmethod
+    def greeting():
+        """Функция приветствия"""
+
+        print("Программа для получения вакансий с сайта hh.ru и работы с ними")
+
+
+    # def get_hh_vacancy():
